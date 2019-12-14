fd3800f1aaf96874e7099cd173c8bebe198a0313
UmbertoV <umberto.ottone@gmail.com>
2019-12-14 17:52:53 +0100

    Chiamata ajax funzionante

--- main.js
+++ main.js
@@ -3,23 +3,39 @@ $(document).ready(function() {
     var templateHtml = $("#template").html();
     var templateFunction = Handlebars.compile(templateHtml);
 
-    var variabili = {
-        copertina : "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
-        album : "New Jersey",
-        artista : "Bon Jovi",
-        anno : "1988"
-    }
-
-
-
-    var htmlFinale = templateFunction(variabili);
-    $(".container").append(htmlFinale);
 
     $.ajax({
          url : "https://flynn.boolean.careers/exercises/api/array/music",
          method : "get",
          success : function(data) {
-             console.log(data);
+             // recupero l'array che contiene tutti i dischi
+
+             var datiDischi = data.response;
+             // ciclo per tutti i dischi
+             for (var i = 0; i < datiDischi.length; i++) {
+                 // recupero il disco corrente nell'iterazione
+                 var datiDisco = datiDischi[i];
+                 console.log(datiDisco);
+
+                 // per ogni disco recupero le varie informazioni
+                 var imgCopertina = datiDisco.poster;
+                 var nomeAlbum = datiDisco.title;
+                 var nomeArtista = datiDisco.author;
+                 var annoUscita = datiDisco.year;
+                 // creo le varianti Handlebars
+                 var variabili = {
+                     copertina : imgCopertina,
+                     album : nomeAlbum,
+                     artista : nomeArtista,
+                     anno : annoUscita
+                 };
+                 console.log(variabili);
+                 // creo il template
+                 var htmlFinale = templateFunction(variabili);
+                 
+                 // lo appendo nel contenitore dei dischi
+                 $(".container").append(htmlFinale);
+             }
          },
          error : function () {}
     });
