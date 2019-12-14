0b9de9209da6ead3f14b63cf693b2cf3fb3c2228
UmbertoV <umberto.ottone@gmail.com>
2019-12-14 17:04:27 +0100

    Impostato l'handlbars... inizio con l'impostazione della chiamata ajax

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
 
+    var templateHtml = $("#template").html();
+    var templateFunction = Handlebars.compile(templateHtml);
+
+    var variabili = {
+        copertina : "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+        album : "New Jersey",
+        artista : "Bon Jovi",
+        anno : "1988"
+    }
+
+
+
+    var htmlFinale = templateFunction(variabili);
+    $(".container").append(htmlFinale);
+
+    $.ajax({
+         url : "https://flynn.boolean.careers/exercises/api/array/music",
+         method : "get",
+         success : function(data) {
+             console.log(data);
+         },
+         error : function () {}
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 });
